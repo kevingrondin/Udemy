dc71cd786599e118a56d33ab43478481720ed181
zonword <kevingrondin@outlook.com>
2017-09-29 11:59:21 +0400

    Update AnalyseAndExtract.sh

--- AnalyseAndExtract.sh
+++ AnalyseAndExtract.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-for i in {1..3}
+for i in {1..10}
 do
   curl -L https://udemycoupon.learnviral.com/coupon-category/development/page/$i/ >> result.txt
   curl -L http://www.anycouponcode.net/category/online-course/udemy/page/$i/ >> result.txt
@@ -12,7 +12,10 @@ done
 curl -L https://www.desidime.com/stores/udemy >> result.txt
 curl -L http://www.promocoupons24.com/search/label/Programming >> result.txt
 
-cat result_tmp.txt | sort | uniq -i > result.txt
-rm result_tmp.txt
+cat result.txt | sort | uniq -i > fin.txt
+rm result.txt
+
+grep -o 'https://www.udemy.com/[[:alnum:]+\.\_\-]*/?[[:alnum:]+\.\_\-]*=[[:alnum:]+\.\_\-]*' fin.txt | sort | uniq -i > urls.txt
+rm fin.txt
 
 exit 0
